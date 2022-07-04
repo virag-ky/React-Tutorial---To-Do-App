4fcddcb4a01e15e48026e387d8c63aff5b3d7b64
Zero to Mastery <vkormoczy@gmail.com>
2022-07-04 12:36:02 +0300

    Add components files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import TodoContainer from "./components/TodoContainer";
 
 function App() {
   return (
     <div className="App">
-      
+      <TodoContainer />
     </div>
   );
 }
